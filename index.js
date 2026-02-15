6f0a1e0a878392e805ce52a6c529c8c8c1f8e29b
mart-code <aamadewoyin@gmail.com>
2026-02-15 19:10:02 +0100

    initalized server for chat app

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+const databaseUrl = process.env.DATABASE_URL;
+
+ app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+mongoose.connect(databaseUrl).then(() => {
+  console.log("Connected to MongoDB");
+}).catch(()=> {
+  console.error("Failed to connect to MongoDB");
+});
+
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.use(cors({
+  origin: process.env.ORIGIN,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  credentials: true,
+}));
+app.use(express.json());
+app.use(cookieParser());
